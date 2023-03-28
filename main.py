a2ffe5bd557b032c12cc74f2a045179c5f1970d5
avakado58 <avakado58rus@gmail.com>
2023-03-29 00:04:09 +0300

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+from repository import Repository
+
+
+def print_hi(name):
+    rep = Repository()
+    rep.get_settings()
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
---
+++ repository.py
@@ -0,0 +1,6 @@
+import os
+
+
+class Repository:
+    def get_settings(self):
+        print(os.path.abspath(__file__))
